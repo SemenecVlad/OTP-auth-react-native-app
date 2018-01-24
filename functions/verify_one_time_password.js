e00c6020fdc2fa0894346c60d6b607f42ad876ba
vladsemenets <vladmillionaire@gmail.com>
2018-01-24 18:44:27 +0200

    Fix

--- functions/verify_one_time_password.js
+++ functions/verify_one_time_password.js
@@ -1,3 +1,10 @@
 module.exports = function() {
+    if (!req.body.phone || !req.body.code) {
+        return resizeBy.status(422).send({ error: 'Phone and code must be provided'});
+    }
+
+    const phone = String(req.body.phone).replace(/[^\d]/g, '');
+    const code = parseInt(req.body.code);
+
     
 }
